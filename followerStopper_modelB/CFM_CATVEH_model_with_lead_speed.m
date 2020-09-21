eee6d26768d9f31624a3ae45a1a075381ca724fe
chou5jerry <fcchou@berkeley.edu>
2020-09-21 15:18:32 -0700

    Simulate model B using field test data

---
+++ followerStopper_modelB/CFM_CATVEH_model_with_lead_speed.m
@@ -0,0 +1,90 @@
+%% 
+function dx = CFM_CATVEH_model_with_lead_speed(t,x,uMin,uMax,params,exp_num)
+
+%% system states
+%d_rel = x(1) : relative distance between lead vehicle and the following vehicle
+%v_follower = x(2) : speed of the follower vehicle
+% auxiliary states
+% x(3) : PID1 state - yi
+% x(4) : PID1 state - YD
+% x(5) : PID2 state - yi
+% x(6) : PID2 state - YD
+
+
+%% system inputs
+% u : acceleration of the following vehicle
+% data_time/data_lead_spd : lead vehicle speed time series
+
+%% parameters 
+% uMin/uMax : acceleraiton lower and upper bound of the follower vehicle
+% non_negative_speed : whether imposing state constraint or not
+
+global data_time data_lead_spd
+v_lead = interp1(data_time,data_lead_spd,t);
+
+non_negative_speed = 1;
+
+dx = zeros(size(x));
+
+d_rel = x(1);
+v_f = x(2);
+v_rel = v_lead-v_f;
+x_vehicle = x(2:6);
+delay_size = params.delay_size;
+if(~isfield(params,'external_r'))
+%     u = dyn_follower_stopper(d_rel,v_rel,v_f,uMin,uMax);
+    [dx_vehicle,~] = dyn_follower_stopper(t,x_vehicle,d_rel,v_rel,uMin,uMax,delay_size,exp_num);
+else
+    if(isempty(params.external_r))
+%         u = dyn_follower_stopper(d_rel,v_rel,v_f,uMin,uMax,v_lead);
+        [dx_vehicle,~] = dyn_follower_stopper(t,x_vehicle,d_rel,v_rel,uMin,uMax,delay_size,exp_num,v_lead);
+    else
+%         u = dyn_follower_stopper(d_rel,v_rel,v_f,uMin,uMax,params.external_r);
+         [dx_vehicle,~] = dyn_follower_stopper(t,x_vehicle,d_rel,v_rel,uMin,uMax,delay_size,exp_num,params.external_r);
+    end
+end
+
+u = dx_vehicle(1);
+
+if(non_negative_speed == 1)
+%     lambda = leadVehAccel(x(1),x(2)-x(3),x(3));
+    kappa = followVehAccel(x(1),v_rel,v_f);
+else
+%     lambda = 1;
+    kappa = 1;
+end
+
+
+dx(1) = v_lead-x(2);
+dx(2) = kappa*u;
+dx(3) = dx_vehicle(2);
+dx(4) = dx_vehicle(3);
+dx(5) = dx_vehicle(4);
+dx(6) = dx_vehicle(5);
+
+end
+
+% function lambda = leadVehAccel(d_rel,v_rel,v_av)
+%     v_lead = v_rel+v_av;
+%     lambda = zeros(size(v_rel));
+%     time_costant = 5;
+%     lambda(v_lead>0) = 1 - exp(-time_costant*v_lead);
+% end
+
+
+function kappa = followVehAccel(d_rel,v_rel,v_av)
+    
+    kappa = zeros(size(v_av));
+    
+    kappa(v_av>0) = 1 ;
+
+end
+
+
+
+
+
+
+
+
+
--- followerStopper_modelB/main_followerStopper_vs_humam.m
+++ followerStopper_modelB/main_followerStopper_vs_humam.m
@@ -1,7 +1,7 @@
-% clear all
-% clc
-% 
-% load_fieldTestData
+clear all
+clc
+
+load_fieldTestData
 
 %% extract field test data : some trajectories
 
@@ -33,6 +33,12 @@ test_data_d_rel = raw_data_d_rel(temp_index_init:temp_index_end);
 test_data_v_lead = raw_data_v_lead(temp_index_init:temp_index_end);
 test_data_v_f = raw_data_v_f(temp_index_init:temp_index_end);
 test_data_time = raw_data_time(temp_index_init:temp_index_end);
+test_data_v_rel = raw_data_v_rel(temp_index_init:temp_index_end);
+
+
+global data_time data_lead_spd
+data_time = test_data_time-test_data_time(1);
+data_lead_spd = test_data_v_lead;
 %% simulate the follower stopper with the field test data
 d_rel_0 = test_data_d_rel(1);
 v_lead_0 = test_data_v_lead(1);
@@ -45,15 +51,14 @@ tspan = [0 30];
 params.delay_size = 0.0;
 params.d_Min = -3.5;
 
-x0 = [d_rel_0;v_lead_0;v_f_0;0;0;0;0]; 
+x0 = [d_rel_0;v_f_0;0;0;0;0]; 
 % opts = odeset('RelTol',1e-10,'AbsTol',1e-10);
 % [t,y] = ode45(@(t,x) CFM_CATVEH_model(t,x,uMin,uMax,params,1),tspan,x0,opts);    
-[t,y] = ode45(@(t,x) CFM_CATVEH_model(t,x,uMin,uMax,params,1),tspan,x0);    
-
+[t,y] = ode45(@(t,x) CFM_CATVEH_model_with_lead_speed(t,x,uMin,uMax,params,1),tspan,x0);    
 
 d_rel = y(:,1);
-v_lead = y(:,2);
-v_f = y(:,3);
+v_f = y(:,2);
+v_lead = interp1(data_time,data_lead_spd,t);
 v_rel = v_lead-v_f;
 
 %compute follower stopper command speed
@@ -73,18 +78,31 @@ hold on
 plot(t,d_rel,'-.','LineWidth',2)
 % plot(t_no_delay,d_rel_no_delay,'-.','LineWidth',2)
 xlabel('Time[s]','FontSize',30)
-ylabel('relative position','FontSize',30)
+ylabel('Relative position[m]','FontSize',30)
 set(gca,'FontSize',30)
 legend('human driver','follower Stopper')
+xlim([0 t(end)])
+
+figure()
+plot(data_time,data_lead_spd,'LineWidth',2)
+hold on
+plot(t,v_f,'-.','LineWidth',2)
+plot(t,v_cmd,'LineWidth',2)
+xlabel('Time[s]','FontSize',30)
+ylabel('Speed[m/s]','FontSize',30)
+set(gca,'FontSize',30)
+legend('human driver','followerStopper','followerStopper_{cmd}')
+xlim([0 t(end)])
 
 %%
 
 fig_handle = followerStopper_boundary()
-plot(test_data_d_rel,test_data_v_rel,'LineWidth',2)
+h1 = plot(test_data_v_rel,test_data_d_rel,'LineWidth',2)
 hold on
-plot(v_rel,d_rel,'-.','LineWidth',2)
-legend('human driver','follower Stopper')
-
+h2 = plot(v_rel,d_rel,'-.','LineWidth',2)
+legend([h1,h2],'human driver','follower Stopper')
+ylabel('Relative position[m]','FontSize',30)
+xlabel('Relative Speed[m/s]','FontSize',30)
 
 %% follower stopper boundary plot
 function fig_handle = followerStopper_boundary()
@@ -108,10 +126,10 @@ function fig_handle = followerStopper_boundary()
     boundary3_x =  w(3) + (0.5*v_rel_start_grid.^2)/alpha(3);
     
     fig_handle = figure()
-    plot(v_rel_grid,boundary1_x)
+    plot(v_rel_grid,boundary1_x,'LineWidth',2)
     hold on
-    plot(v_rel_grid,boundary2_x)
-    plot(v_rel_grid,boundary3_x)
+    plot(v_rel_grid,boundary2_x,'LineWidth',2)
+    plot(v_rel_grid,boundary3_x,'LineWidth',2)
     set(gca,'FontSize',30)
     xlabel('rel spd [m/s]','FontSize',30)
     ylabel('rel dist[m]','FontSize',30)
