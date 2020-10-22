d68417fe0ca47cdf54c2f97e15302a7137b52567
chou5jerry <fcchou@berkeley.edu>
2020-10-21 17:23:40 -0700

    update code to incorporate results of 3D generic model

--- compare_modelA_generic3D/main_conparing_FS_safetySet_vs_3D_SafetySet_Value_Driving_Data.m
+++ compare_modelA_generic3D/main_conparing_FS_safetySet_vs_3D_SafetySet_Value_Driving_Data.m
@@ -33,34 +33,23 @@ valid_relative_spd = relative_speed(speed_lead>0 & relative_distance<200);
 valid_speed = speed(speed_lead>0& relative_distance<200);
 valid_accel = accel(speed_lead>0 & relative_distance<200);
 
-%%
-%% HJI 2D safety set
-% [XX_2D,YY_2D,ValFunc_2D] = HJI_2D_safety_value_function_generator();
-load('value_function_2D_SafetySet.mat')
 
 %%
 %% load follower stopper safety set 
-load('value_function_modelA.mat')
+load('value_function_3D_generic_SafetySet.mat')
 
 %% convert coordinate from (d_rel,v_lead,v_f) to (de_rel,v_rel,v_f)
-x_grid = d_rel_g; %0:2:50
-y_grid = v_lead_g; %0:1:30;
-z_grid = v_follower_g; %0:1:30;
+[safetyValue_3dGeneric] = absolute2relative(safetySet_3dGeneric.x_new_grid,safetySet_3dGeneric.y_new_grid,safetySet_3dGeneric.z_new_grid,-safetySet_3dGeneric.new_val_func);
 
-x_origin = x_grid;
-y_origin = y_grid;
-z_origin = z_grid;
-v_origin = min_gap_matrix;
-[followerStopper_safetyValue] = absolute2relative(x_origin,y_origin,z_origin,v_origin);
+x_grid_3dGeneric = safetyValue_3dGeneric.x_new_grid;
+y_grid_3dGeneric = safetyValue_3dGeneric.y_new_grid;
+z_grid_3dGeneric = safetyValue_3dGeneric.z_new_grid;
+val_func_3dGeneric = safetyValue_3dGeneric.new_val_func;
 
-x_new_grid = followerStopper_safetyValue.x_new_grid;
-y_new_grid = followerStopper_safetyValue.y_new_grid;
-z_new_grid = followerStopper_safetyValue.z_new_grid;
-new_val_func = followerStopper_safetyValue.new_val_func;
 
 %% plot value function in the new coordinate
-[XX,YY,ZZ] = meshgrid(x_new_grid,y_new_grid,z_new_grid);
-V = permute(new_val_func,[2 1 3]);
+[XX,YY,ZZ] = meshgrid(x_grid_3dGeneric,y_grid_3dGeneric,z_grid_3dGeneric);
+V = permute(val_func_3dGeneric,[2 1 3]);
 
 figure_3dsafetySet_handle2 = figure()
 P = patch(isosurface(XX,YY,ZZ,V,0));
@@ -83,21 +72,21 @@ grid on
 %% HJI 3D Generic safety set
 % load('value_function_3D_generic_SafetySet.mat')
 % load('value_function_3D_generic_SafetySet_robust_1021.mat')
-load('value_function_FS_tool_box.mat')
+load('value_function_FS_modelA_toolbox.mat')
 
-new_grid.x = x_new_grid;
-new_grid.y = y_new_grid;
-new_grid.z = z_new_grid;
-[safetyValue_3dGeneric] = absolute2relative(safetySet_3dGeneric.x_new_grid,safetySet_3dGeneric.y_new_grid,safetySet_3dGeneric.z_new_grid,-safetySet_3dGeneric.new_val_func,new_grid);
+% new_grid.x = x_new_grid;
+% new_grid.y = y_new_grid;
+% new_grid.z = z_new_grid;
+[safetyValue_FS] = absolute2relative(safetySet_3dGeneric.x_new_grid,safetySet_3dGeneric.y_new_grid,safetySet_3dGeneric.z_new_grid,-safetySet_3dGeneric.new_val_func);
 
-x_grid_3dGeneric = safetyValue_3dGeneric.x_new_grid;
-y_grid_3dGeneric = safetyValue_3dGeneric.y_new_grid;
-z_grid_3dGeneric = safetyValue_3dGeneric.z_new_grid;
-val_func_3dGeneric = safetyValue_3dGeneric.new_val_func;
+x_grid_FS = safetyValue_FS.x_new_grid;
+y_grid_FS = safetyValue_FS.y_new_grid;
+z_grid_FS = safetyValue_FS.z_new_grid;
+val_func_FS = safetyValue_FS.new_val_func;
 
 %% plot value function 3D Generic in the new coordinate
-[XX,YY,ZZ] = meshgrid(x_grid_3dGeneric,y_grid_3dGeneric,z_grid_3dGeneric);
-V = permute(val_func_3dGeneric,[2 1 3]);
+[XX,YY,ZZ] = meshgrid(x_grid_FS,y_grid_FS,z_grid_FS);
+V = permute(val_func_FS,[2 1 3]);
 
 figure_3dGenericsafetySet_handle = figure()
 P = patch(isosurface(XX,YY,ZZ,V,0));
@@ -116,8 +105,8 @@ title('safety set - 3D generic model')
 set(gca,'FontSize',20)
 grid on
 
-[XX,YY,ZZ] = meshgrid(x_new_grid,y_new_grid,z_new_grid);
-V = permute(new_val_func,[2 1 3]);
+[XX,YY,ZZ] = meshgrid(x_grid_3dGeneric,y_grid_3dGeneric,z_grid_3dGeneric);
+V = permute(val_func_3dGeneric,[2 1 3]);
 Q = patch(isosurface(XX,YY,ZZ,V,0));
 isonormals(XX,YY,ZZ,V,P)
 Q.FaceColor = 'blue';
@@ -126,15 +115,17 @@ Q.EdgeColor = 'none';
 
 %%
 %% plot safety boundary at each speed level
-[XX_3dGeneric,YY_3dGeneric] = meshgrid(x_grid_3dGeneric,y_grid_3dGeneric);
-safetySlice_3dGeneric = val_func_3dGeneric(:,:,1);
+
 
 figure_3DsafetySet_On2D_handle = figure()
-[XX,YY] = meshgrid(x_new_grid,y_new_grid);
-safetySlice = new_val_func(:,:,1);
-[~,contourPlot_handle_3d] = contour(YY,XX,safetySlice',[0 0],'LineWidth',2);
+[XX_FS,YY_FS] = meshgrid(x_grid_FS,y_grid_FS);
+safetySlice_FS = val_func_FS(:,:,1);
+[~,contourPlot_handle_3d] = contour(YY_FS,XX_FS,safetySlice_FS',[0 0],'LineWidth',2);
 contourPlot_handle_3d.Color = [0.9290, 0.6940, 0.1250];
 hold on
+
+[XX_3dGeneric,YY_3dGeneric] = meshgrid(x_grid_3dGeneric,y_grid_3dGeneric);
+safetySlice_3dGeneric = val_func_3dGeneric(:,:,1);
 [~,contourPlot_handle_3dGeneric] = contour(YY_3dGeneric,XX_3dGeneric,safetySlice_3dGeneric',[0 0],'LineWidth',2);
 contourPlot_handle_3dGeneric.Color = [0.4940 0.1840 0.5560];
 
@@ -146,12 +137,12 @@ xlim([-15 15])
 ylim([-5 120])
 v_r_boundary_handle = plot([0,0],[0,120],'LineWidth',2,'Color','black');
 
-%overlay 2D safety set
-M_2D = contour(YY_2D,XX_2D,ValFunc_2D,[0 0]);
-plot(M_2D(1,2:end),M_2D(2,2:end),'LineWidth',2,'Color',[0    0.4470    0.7410])
-
 %overlay driving data points
 plot_handle_driving_data = scatter(valid_relative_spd,valid_relative_dist,'.','MarkerFaceAlpha',1.0,'MarkerEdgeAlpha',1.0,'MarkerFaceColor',[0.4660 0.6740 0.1880],'MarkerEdgeColor',[0.4660 0.6740 0.1880]);
+
+v_follower_g = z_grid_3dGeneric;
+
+legend('FS','3D')
 
 fname = 'FS_safetySet_vs_2D_vs_fieldTestData.gif';
 for frame_index = 1:1:length(v_follower_g)
@@ -165,8 +156,8 @@ for frame_index = 1:1:length(v_follower_g)
     plot_handle_driving_data.XData = relative_spd_pt_temp;
     plot_handle_driving_data.YData = relative_dist_pt_temp;    
     
-    safetySlice = new_val_func(:,:,frame_index);
-    contourPlot_handle_3d.ZData = safetySlice';
+    safetySlice_FS = val_func_FS(:,:,frame_index);
+    contourPlot_handle_3d.ZData = safetySlice_FS';
     safetySlice_3dGeneric = val_func_3dGeneric(:,:,frame_index);
     contourPlot_handle_3dGeneric.ZData = safetySlice_3dGeneric';
     v_r_boundary_handle.XData=[-v_follower_g(frame_index),-v_follower_g(frame_index)];
